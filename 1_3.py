cd9b8b96a9060d6285b2f89aa6b44dc072ab1d0e
Vera <antimouse@inbox.ru>
2021-07-28 22:10:36 +0300

    выполнила практические задания к уроку 1

---
+++ 1_3.py
@@ -0,0 +1,15 @@
+user_number = (input('Введите любое число'))
+
+# Найдите сумму чисел n + nn + nnn
+
+n = user_number
+nn = user_number + user_number
+nnn = user_number + user_number + user_number
+
+# print(n, nn, nnn)
+
+number = int(n) + int(nn) + int(nnn)
+print(number)
+
+
+
