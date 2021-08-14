058e66d3b8d11401f5255601f8e276588c0375d5
Vera Kim <87915604+Antimouse12@users.noreply.github.com>
2021-08-14 03:02:42 +0300

    Delete 5_2.py

--- lesson_5/5_2.py
+++
@@ -1,8 +0,0 @@
-with open('text_1.txt') as f:
-    lines = f.readlines()
-    lines_count = len(lines)
-    print(f'количество строк в файле: {lines_count}')
-
-    for index, value in enumerate(lines):
-        words_count = len(value.split())
-        print(f'количество слов в {index} строке: {words_count}')
